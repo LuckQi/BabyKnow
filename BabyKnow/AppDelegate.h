17a3c782ab8193f9a6bacbe9b2ac1da467ed1870
user <user@userdemacbook-pro.local>
2016-03-23 09:10:27 +0800

    Initial Commit

---
+++ BabyKnow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyKnow
+//
+//  Created by user on 16/3/23.
+//  Copyright © 2016年 BoCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabyKnow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabyKnow
+//
+//  Created by user on 16/3/23.
+//  Copyright © 2016年 BoCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
